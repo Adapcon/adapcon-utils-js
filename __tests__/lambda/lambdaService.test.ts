2423a789579be0720b32bfeb0c0a8bde289c77b4
caroltrindade <carol.setembro@hotmail.com>
2021-10-25 15:43:28 -0300

    feat: added lambda formatters

---
+++ __tests__/lambda/lambdaService.test.ts
@@ -0,0 +1,38 @@
+import { LambdaService } from '../../src/lambda';
+
+describe('invoke', () => {
+  it('Should returns the function invoked response', async () => {
+    await expect(LambdaService.invoke({
+      port: '13011',
+      functionName: 'authenticate-api-generalValidateSession',
+      headers: {
+        apikey: 'apikey',
+      },
+      pathParameters: {
+        app: 'simplificamais',
+        code: '',
+      },
+      isOffline: true,
+    })).resolves.toEqual({status: 400, body: { code:  'undefined' }});
+  });
+
+  it('Should returns the function invoked with error', async () => {
+    await expect(LambdaService.invoke({
+      functionName: 'authenticate-api-generalValidateSession',
+      headers: {
+        apikey: 'apikey',
+      },
+      pathParameters: {
+        app: 'simplificamais',
+        code: '',
+      },
+      isOffline: false,
+    })).rejects.toBeTruthy();
+  });
+
+  it('Should returns error to invoke without optional properties', async () => {
+    await expect(LambdaService.invoke({
+      functionName: 'authenticate-api-generalValidateSession',
+    })).rejects.toBeTruthy();
+  });
+});
---
+++ src/lambda/lambdaService.ts
@@ -0,0 +1,64 @@
+import AWS from 'aws-sdk';
+
+import { formattedResponse } from './formatters';
+
+export class LambdaService {
+  static async invoke({
+    port = '',
+    region = 'us-east-1',
+    functionName,
+    invocationType = 'RequestResponse',
+    headers = {},
+    body = {},
+    pathParameters = {},
+    queryStringParameters = {},
+    isOffline = false,
+  }: {
+    port?: string,
+    region?: string,
+    functionName: string,
+    invocationType?: string,
+    headers?: object,
+    body?: object,
+    pathParameters?: object,
+    queryStringParameters?: object,
+    isOffline?: boolean,
+  }): Promise<object> {
+    try {
+      const lambda = new AWS.Lambda({
+        region,
+        ...(isOffline ? {
+          region: 'localhost',
+          endpoint: `http://localhost:${port}`,
+        } : {}),
+      });
+
+      const response = await lambda.invoke({
+        FunctionName: functionName,
+        InvocationType: invocationType,
+        Payload: Buffer.from(
+          JSON.stringify({
+            headers,
+            body,
+            pathParameters,
+            queryStringParameters,
+          }),
+        ),
+      }).promise();
+
+      return formattedResponse(response);
+    } catch (error) {
+      console.log('Error LambdaService invoke', error);
+      throw error;
+    }
+  }
+
+  // static formattedResponse({ StatusCode, Payload }: { StatusCode?: any, Payload?: any }): object {
+  //   const payloadFormatted = JSON.parse(Payload || '{}');
+
+  //   return {
+  //     status: payloadFormatted.statusCode || StatusCode,
+  //     body: payloadFormatted.body ? JSON.parse(payloadFormatted.body) : {},
+  //   };
+  // }
+};
