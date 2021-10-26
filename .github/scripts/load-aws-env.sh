191530efd08ba02a8ee5e5eaa8a24b539934f9b1
caroltrindade <carol.setembro@hotmail.com>
2021-10-26 15:35:25 -0300

    fix: removed spaces

--- .github/scripts/load-aws-env.sh
+++ .github/scripts/load-aws-env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-AWS_ACCESS_KEY_ID = $(echo "$1" | jq -r ".SIMPLIFICAMAIS_PRD_AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = $(echo "$1" | jq -r ".SIMPLIFICAMAIS_PRD_AWS_SECRET_ACCESS_KEY")
-AWS_REGION = "sa-east-1"
+AWS_ACCESS_KEY_ID=$(echo "$1" | jq -r ".SIMPLIFICAMAIS_PRD_AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY=$(echo "$1" | jq -r ".SIMPLIFICAMAIS_PRD_AWS_SECRET_ACCESS_KEY")
+AWS_REGION="sa-east-1"
 
 if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
   echo "::error ::undefined variables"
