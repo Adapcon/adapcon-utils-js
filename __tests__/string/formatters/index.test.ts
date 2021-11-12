0be43090c73ec8aa5dfdf21912b5f753f8c31dfb
Alan Neves <38226523+alanneves@users.noreply.github.com>
2021-11-12 08:12:44 -0300

    test: adjusts test

--- __tests__/string/formatters/index.test.ts
+++ __tests__/string/formatters/index.test.ts
@@ -3,7 +3,7 @@ import { capitalizeFirstLetter } from '../../../src/string'
 describe('capitalizeFirstLetter', () => {
   const data = [{input: 'teste', output: 'Teste'}, {input: 'TESTE', output: 'TESTE'}, {input: 'alan Reno Neves', output: 'Alan Reno Neves'}]
 
-  test.each(data)('Should return true if parameter is string', (param) => {
+  test.each(data)('Should return the word with the first letter capitalize', (param) => {
     expect(capitalizeFirstLetter(param.input)).toBe(param.output)
   })
 })
