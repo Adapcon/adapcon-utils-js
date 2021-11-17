f381cadc431e621be2c0fbe95060b43c021c9897
Alan Neves <alanrenoneves@gmail.com>
2021-11-17 13:50:59 -0300

    feat: formatPhone function

---
+++ __tests__/phone/formatters/index.test.ts
@@ -0,0 +1,17 @@
+import { formatPhone } from '../../../src/phone'
+
+describe('formatPhone', () => {
+  const data = [
+    { input: '123', output: '123' },
+    { input: '47996919816', output: '(47) 99691-9816' },
+    { input: '4799616039', output: '(47) 9961-6039' },
+    { input: '99616039', output: '9961-6039' },
+    { input: '999616039', output: '99961-6039' },
+    { input: '(47)99719283', output: '(47) 9971-9283' },
+    { input: '', output: '' }
+  ]
+
+  test.each(data)('Should return the word with the first letter capitalize', (param) => {
+    expect(formatPhone(param.input)).toBe(param.output)
+  })
+})
