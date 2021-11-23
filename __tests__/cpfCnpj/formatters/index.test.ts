01106ad2824c86030fbd2984d6a532611fef69e6
caroltrindade <carol.setembro@hotmail.com>
2021-11-23 09:18:43 -0300

    feat: added cpf formatter

--- __tests__/cpfCnpj/formatters/index.test.ts
+++ __tests__/cpfCnpj/formatters/index.test.ts
@@ -1,4 +1,4 @@
-import { formatCnpj } from '../../../src/cpfCnpj'
+import { formatCnpj, formatCpf } from '../../../src/cpfCnpj'
 
 describe('formatCnpj', () => {
   const validCnpj = [
@@ -19,5 +19,27 @@ describe('formatCnpj', () => {
   })
   test.each(invalidCnpj)('Should return a empty string', (param) => {
     expect(formatCnpj(param.input)).toBe(param.output)
+  })
+})
+
+describe('formatCpf', () => {
+  const validCpf = [
+    { input: '47230396000', output: '472.303.960-00' },
+    { input: '75330268028', output: '753.302.680-28' },
+    { input: '67738058056', output: '677.380.580-56' }
+  ]
+  const invalidCpf = [
+    { input: '47991039600183', output: '' },
+    { input: '479980796', output: '' },
+    { input: '572.203.960-01', output: '' },
+    { input: '584120044425635158745500', output: '' },
+    { input: '', output: '' }
+  ]
+
+  test.each(validCpf)('Should return the cpf formatted', (param) => {
+    expect(formatCpf(param.input)).toBe(param.output)
+  })
+  test.each(invalidCpf)('Should return a empty string', (param) => {
+    expect(formatCpf(param.input)).toBe(param.output)
   })
 })
