5aa0a7ac8dd34487c5e6910ff6eb5759afd8df13
Marcos Rodrigo Jung Alves <digosw@gmail.com>
2021-12-14 08:08:01 -0300

    fix: typed function don't need to verify if first param is object

--- src/lambda/lambdaCheckParameters.ts
+++ src/lambda/lambdaCheckParameters.ts
@@ -3,15 +3,11 @@ import type { LambdaErrors } from './interfaces'
 export const lambdaCheckParameters = (object: object, indexes: string[]): object => {
   const errors: LambdaErrors = {}
   if (Object.keys(object).length > 0 && indexes.length > 0) {
-    if (typeof object === 'object' && object !== null) {
-      indexes.forEach(index => {
-        errors[index] = { ...(!object[index] ? { error: 'undefined' } : '') }
-        // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-        if (index in object) delete errors[index]
-      })
-    } else {
-      errors.param = 'param sent is not an Object'
-    }
+    indexes.forEach(index => {
+      errors[index] = { ...(!object[index] ? { error: 'undefined' } : '') }
+      // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+      if (index in object) delete errors[index]
+    })
   } else {
     errors.param = 'Object is empty'
   }
