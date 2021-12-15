ebc5d9fde0853d3139b43e90970dcb068359c50b
Marcos Rodrigo Jung Alves <digosw@gmail.com>
2021-12-15 17:17:21 -0300

    chore: reorganizing date folder (#40)
    * chore: reorganizing date folder
    
    * feat: change test name

---
+++ src/date/enums.ts
@@ -0,0 +1,9 @@
+export enum DurationTypes {
+  day = 'Date',
+  month = 'Month',
+  year = 'FullYear',
+  hour = 'Hours',
+  minute = 'Minutes',
+  second = 'Seconds',
+  millisecond = 'Milliseconds'
+}
