90bfc996bb2cf71790b69af9c256f0d1b30b257c
Jeferson Alves <jeferson.a.oficial@gmail.com>
2021-12-15 16:50:17 -0300

    chore: reorganizing object folder

--- src/object/index.ts
+++ src/object/index.ts
@@ -1,7 +1,7 @@
-import { isString } from '../string';
+import { isString } from '../string'
 
-export const isObject = (obj: any) => {
-  return typeof obj === 'object' && obj === Object(obj) && !Array.isArray(obj);
+export const isObject = (obj: any): boolean => {
+  return typeof obj === 'object' && obj === Object(obj) && !Array.isArray(obj)
 }
 
-export const objToStr = (arg: any) => ((isString(arg)) ? arg : JSON.stringify(arg));
+export const objToStr = (arg: any): string => ((isString(arg)) ? arg : JSON.stringify(arg))
