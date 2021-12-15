f3702c4adbd1bf2408a8afacfbe232212950e541
Marcos Rodrigo Jung Alves <digosw@gmail.com>
2021-12-15 11:09:59 -0300

    fix: moving tests to the new file

--- __tests__/lambda/lambdaResponses.test.ts
+++ __tests__/lambda/lambdaResponses.test.ts
@@ -101,3 +101,31 @@ describe('lambdaRespError', () => {
     expect(lambdaRespError(param.error)).toStrictEqual(param.response)
   })
 })
+
+it('Should returns lambda response formatted with a body string', () => {
+  expect(lambdaResp(201, 'Created')).toEqual({ body: 'Created', statusCode: 201 })
+})
+
+it('Should returns lambda response formatted with a body object', () => {
+  expect(lambdaResp(201, { user: 'Created' })).toEqual({ body: '{"user":"Created"}', statusCode: 201 })
+})
+
+it('Should returns lambda response error formatted with status and message', () => {
+  expect(lambdaRespError({ status: 404, message: 'User Not Found' })).toEqual({ statusCode: 404, body: '{"error":"User Not Found"}' })
+})
+
+it('Should returns lambda response error formatted with status code and error', () => {
+  expect(lambdaRespError({ statusCode: 404, error: { user: 'Not Found' } })).toEqual({ statusCode: 404, body: '{"error":{"user":"Not Found"}}' })
+})
+
+it('Should returns lambda response error formatted without status', () => {
+  expect(lambdaRespError({ message: 'Invalid field' })).toEqual({ statusCode: 500, body: '{"error":"Invalid field"}' })
+})
+
+it('Should returns lambda response error formatted without message', () => {
+  expect(lambdaRespError({ statusCode: 500 })).toEqual({ statusCode: 500, body: '' })
+})
+
+it('Should returns lambda response error formatted without status and message', () => {
+  expect(lambdaRespError({})).toEqual({ statusCode: 500, body: '' })
+})
