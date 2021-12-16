fe6963a85e5503af5df821dd61130cd92eae1c94
GabrielPedrotti <gabrielps2003@gmail.com>
2021-12-16 14:55:16 -0300

    fix: function name

--- src/string/formatters.ts
+++ src/string/formatters.ts
@@ -1,3 +1,3 @@
 export const capitalizeFirstLetter = (str: string): string => str.charAt(0).toUpperCase() + str.slice(1)
 
-export const camelize = (str: string): string => str.replace(/-./g, x=>x[1].toUpperCase())
+export const kebabCaseToCamelCase = (str: string): string => str.replace(/-./g, x=>x[1].toUpperCase())
