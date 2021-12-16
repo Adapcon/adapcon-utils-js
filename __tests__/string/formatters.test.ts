f9d44d3d48b4a9c0523f85ec088dba9c8378e600
GabrielPedrotti <78749782+GabrielPedrotti@users.noreply.github.com>
2021-12-16 15:40:28 -0300

    Feature/created new kebab case to camel case formatter (#52)
    * feat: created new kebab-case to camel case formatter
    
    * fix: function name
    
    * fix: created unit tests
    
    * Update __tests__/string/formatters.test.ts
    
    Co-authored-by: Jeferson Alves <jeferson.a.oficial@gmail.com>
    
    * fix: multiple lines
    
    Co-authored-by: Jeferson Alves <jeferson.a.oficial@gmail.com>

--- __tests__/string/formatters.test.ts
+++ __tests__/string/formatters.test.ts
@@ -1,9 +1,19 @@
-import { capitalizeFirstLetter } from '../../src/string'
+import { capitalizeFirstLetter, kebabCaseToCamelCase } from '../../src/string'
 
 describe('capitalizeFirstLetter', () => {
   const data = [{ input: 'teste', output: 'Teste' }, { input: 'TESTE', output: 'TESTE' }, { input: 'alan Reno Neves', output: 'Alan Reno Neves' }]
 
   test.each(data)('Should return the word with the first letter capitalize', (param) => {
     expect(capitalizeFirstLetter(param.input)).toBe(param.output)
+  })
+})
+
+describe('kebabCaseToCamelCase', () => {
+  const data = [{ input: 'teste-adapcon', output: 'testeAdapcon' },
+    { input: 'TESTE', output: 'TESTE' },
+    { input: 'alan-Reno-Neves', output: 'alanRenoNeves' }]
+
+  test.each(data)('Should return the kebab-case string in camel case', (param) => {
+    expect(kebabCaseToCamelCase(param.input)).toBe(param.output)
   })
 })
--- src/string/formatters.ts
+++ src/string/formatters.ts
@@ -1,1 +1,3 @@
 export const capitalizeFirstLetter = (str: string): string => str.charAt(0).toUpperCase() + str.slice(1)
+
+export const kebabCaseToCamelCase = (str: string): string => str.replace(/-./g, x=>x[1].toUpperCase())
