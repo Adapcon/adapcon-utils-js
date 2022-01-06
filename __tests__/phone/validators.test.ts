d17de47aa16eba926173640dd5cc24509158df9c
Alan Neves <alanrenoneves@gmail.com>
2022-01-06 11:17:24 -0300

    test: change function name

--- __tests__/phone/validators.test.ts
+++ __tests__/phone/validators.test.ts
@@ -1,16 +1,16 @@
-import { isBrazilianCellPhone } from '../../src/phone'
+import { isBrazilianCellPhoneFormat } from '../../src/phone'
 
 describe('isBrazilianPhone', () => {
   const validPhones = [47996184675, 4794567456, '47995643784', '479956-4378', '47 99565-3403', '47 9565-3453', '47 95653453', '47 995653453'
   ]
 
   test.each(validPhones)('Should return true if the phone is in the brazilian format', (phone) => {
-    expect(isBrazilianCellPhone(phone)).toBeTruthy()
+    expect(isBrazilianCellPhoneFormat(phone)).toBeTruthy()
   })
 
   const invalidPhones = ['96969696', '321332233221', '47 9969146784', '47 9 96919817', 12345, undefined, '4799736784a']
 
   test.each(invalidPhones)('Should return false if the phone is not in the brazilian format', (notPhone) => {
-    expect(isBrazilianCellPhone(notPhone as any)).toBeFalsy()
+    expect(isBrazilianCellPhoneFormat(notPhone as any)).toBeFalsy()
   })
 })
