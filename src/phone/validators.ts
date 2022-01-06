686023286a64a4321c322161006e69b835b172be
Alan Neves <alanrenoneves@gmail.com>
2022-01-06 11:17:12 -0300

    feat: change function name

--- src/phone/validators.ts
+++ src/phone/validators.ts
@@ -2,7 +2,7 @@
  * Validates the phone number based in the brazilian phone number format
  * You can't use a phone number with country code
  */
-export const isBrazilianCellPhone = (arg: string | number): boolean => {
+export const isBrazilianCellPhoneFormat = (arg: string | number): boolean => {
   if (!arg || (typeof arg !== 'string' && typeof arg !== 'number')) return false
 
   const regex = /^\(?\d{2}\)?[\s-]?[\s9]?\d{4}-?\d{4}$/
