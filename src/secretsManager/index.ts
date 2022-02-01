0ff597dd865d3608c88d984bc3fbe1df0acf39f7
viserion77 <jeferson.a.oficial@gmail.com>
2022-02-01 17:09:09 -0300

    fix: apply eslint

--- src/secretsManager/index.ts
+++ src/secretsManager/index.ts
@@ -5,10 +5,10 @@ export const SecretManager = {
     const client = new AWS.SecretsManager({ region })
 
     // The SDK can also return a promise calling `promise`
-    const secret = await client.getSecretValue({SecretId: secretId}).promise()
+    const secret = await client.getSecretValue({ SecretId: secretId }).promise()
 
-    if (!secret) throw new Error("Secret not found!")
-    if (!secret.SecretString) throw new Error("Secret without a value!")
+    if (!secret) throw new Error('Secret not found!')
+    if (!secret.SecretString) throw new Error('Secret without a value!')
 
     return JSON.parse(secret.SecretString)
   }
