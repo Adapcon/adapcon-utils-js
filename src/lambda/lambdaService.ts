606f5dfccc17f8598e1297b1d32b1c97a39a31fe
Jeferson Alves <jeferson.a.oficial@gmail.com>
2022-02-01 17:40:38 -0300

    Feature/secret manager (#73)
    * feat: create new function
    
    * feat: export new functions
    
    * chore: ignore aws tests
    
    * chore: improve version
    
    * chore: npm i
    
    * fix: change paramter pattern
    
    * feat: use SecretManager.getValue on LambdaService
    
    * fix: ignore covarege
    
    * refactor: apply code sugestion
    
    * fix: apply eslint
    
    * fix: create a new method to invok lambda

--- src/lambda/lambdaService.ts
+++ src/lambda/lambdaService.ts
@@ -1,58 +1,80 @@
 import AWS from 'aws-sdk'
 
 import { formattedResponse } from './formatters'
+import { SecretManager } from './../secretsManager/index'
+import { lambdaParameters } from '.'
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class LambdaService {
-  static async invoke ({
-    port = '',
-    region = 'sa-east-1',
-    functionName,
-    invocationType = 'RequestResponse',
-    headers = {},
-    body = {},
-    pathParameters = {},
-    queryStringParameters = {},
-    isOffline = false
-  }: {
-    port?: string
-    region?: string
-    functionName: string
-    invocationType?: string
-    headers?: object
-    body?: object
-    pathParameters?: object
-    queryStringParameters?: object
-    isOffline?: boolean
-  }): Promise<object> {
+  static async invoke (lambdaParameters: lambdaParameters): Promise<object> {
     try {
-      const lambda = new AWS.Lambda({
-        region,
-        ...(isOffline
-          ? {
-              region: 'localhost',
-              endpoint: `http://localhost:${port}`
-            }
-          : {})
+      return executeInvoke(lambdaParameters)
+    } catch (error) {
+      console.log('Error LambdaService invoke', error)
+      throw error
+    }
+  }
+
+  static async invokeBySecret (lambdaParameters: {
+    serviceSecretArn: string
+  } & lambdaParameters): Promise<object> {
+    try {
+      const secretValue: {
+        accessKeyId?: string
+        secretAccessKey?: string
+      } = await SecretManager.getValue({ region: lambdaParameters.region, secretId: lambdaParameters.serviceSecretArn })
+      return executeInvoke({
+        ...lambdaParameters,
+        accessKeyId: secretValue.accessKeyId,
+        secretAccessKey: secretValue.secretAccessKey
       })
-
-      const response = await lambda.invoke({
-        FunctionName: functionName,
-        InvocationType: invocationType,
-        Payload: Buffer.from(
-          JSON.stringify({
-            headers,
-            body,
-            pathParameters,
-            queryStringParameters
-          })
-        )
-      }).promise()
-
-      return formattedResponse(response)
     } catch (error) {
       console.log('Error LambdaService invoke', error)
       throw error
     }
   }
 };
+
+const executeInvoke = async ({
+  accessKeyId,
+  secretAccessKey,
+  port = '',
+  region = 'sa-east-1',
+  functionName,
+  invocationType = 'RequestResponse',
+  headers = {},
+  body = {},
+  pathParameters = {},
+  queryStringParameters = {},
+  isOffline = false
+}: {
+  accessKeyId?: string
+  secretAccessKey?: string
+} & lambdaParameters) => {
+  const lambda = new AWS.Lambda({
+    region,
+    accessKeyId,
+    secretAccessKey,
+    ...(isOffline
+      ? {
+          region: 'localhost',
+          endpoint: `http://localhost:${port}`
+        }
+      : {})
+  })
+
+  const response = await lambda.invoke({
+    FunctionName: functionName,
+    InvocationType: invocationType,
+    Payload: Buffer.from(
+      JSON.stringify({
+        headers,
+        body,
+        pathParameters,
+        queryStringParameters
+      })
+    )
+  }).promise()
+
+  return formattedResponse(response)
+}
