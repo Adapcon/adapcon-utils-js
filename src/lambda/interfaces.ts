ad5597987007af7bb5c716592d1fce0586d7118e
Marcos Rodrigo Jung Alves <digosw@gmail.com>
2022-02-07 11:48:43 -0300

    feat: add new crudInputParams type

--- src/lambda/interfaces.ts
+++ src/lambda/interfaces.ts
@@ -4,3 +4,16 @@ export interface Error {
   error?: any
   message?: string
 }
+
+export type CrudInputParams = {
+  httpMethod: 'GET' | 'POST' | 'PUT' | 'DELETE'
+  sort?: string
+  limit?: string
+  page?: string
+  columns?: string
+  onlyCount?: boolean
+  entity?: {[key: string]: any}
+  filters?: {[key: string]: any}
+  keys?: {[key: string]: any}
+  customParameters?: {[key: string]: any}
+}
