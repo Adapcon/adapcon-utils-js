90340d95436ea9d1585e61d91a941599e7075f47
Alan Neves <alanrenoneves@gmail.com>
2022-02-28 08:56:37 -0300

    feat: add removeSpecialCharacters function

--- src/string/formatters.ts
+++ src/string/formatters.ts
@@ -1,3 +1,5 @@
 export const capitalizeFirstLetter = (str: string): string => str.charAt(0).toUpperCase() + str.slice(1)
 
 export const kebabCaseToCamelCase = (str: string): string => str.replace(/-./g, x => x[1].toUpperCase())
+
+export const removeSpecialCharacters = (str: string): string => str.replace(/[^a-zA-Z0-9 ]/g, '')
