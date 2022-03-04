4e2fd1dd592d6e3b50a97e78890476c50ddf1438
Gabriel de Oliveira <gabriel.o03@aluno.ifsc.edu.br>
2022-03-04 11:32:47 -0300

    chore: ignore .docusaurus

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/gabriel/Projects/adapcon-utils-js/website/node_modules/infima/dist/css/default/default.css'),
-  require('/home/gabriel/Projects/adapcon-utils-js/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/gabriel/Projects/adapcon-utils-js/website/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/home/gabriel/Projects/adapcon-utils-js/website/src/css/custom.css'),
-];
