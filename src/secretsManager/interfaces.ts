4a8a414e1caec478cb7ec47c2af5d1b91eb142f7
Marcos Rodrigo Jung Alves <digosw@gmail.com>
2022-03-04 17:51:19 -0300

    feat: add AccessKey and AccessKeyParam on secretsManager interfaces

---
+++ src/secretsManager/interfaces.ts
@@ -0,0 +1,10 @@
+export interface AccessKey {
+  accessKeyId: string
+  secretAccessKey: string
+  region?: string
+}
+
+export interface AccessKeyParam {
+  region?: string
+  serviceSecretArn?: string
+}
