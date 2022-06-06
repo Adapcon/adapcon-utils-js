f0f4a96d54ad5e78c54bc3cfb6ce464fec879fab
Carolina Trindade <32417804+caroltrindade@users.noreply.github.com>
2022-06-06 11:23:00 +0200

    feature/date-formatter-by-date (#141)
    * feat: added date formatter by date
    
    * chore: adjusted tests

--- src/date/formatters.ts
+++ src/date/formatters.ts
@@ -1,5 +1,18 @@
-export const formatDate = (date: string, format: string = 'DD/MM/YYYY'): string => {
-  const [year, month, day] = date.split('-')
+export const formatDate = (date: string | Date, format: string = 'DD/MM/YYYY'): string => {
+  let year: string | number = ''
+  let month: string | number = ''
+  let day: string | number = ''
 
-  return format.replace('DD', day).replace('MM', month).replace('YYYY', year)
+  if (date instanceof Date) {
+    day = date.getDate()
+    month = date.getMonth() + 1
+    year = date.getFullYear()
+
+    if (day < 10) day = `0${day}`
+    if (month < 10) month = `0${month}`
+  } else {
+    [year, month, day] = date.split('-')
+  }
+
+  return format.replace('DD', String(day)).replace('MM', String(month)).replace('YYYY', String(year))
 }
