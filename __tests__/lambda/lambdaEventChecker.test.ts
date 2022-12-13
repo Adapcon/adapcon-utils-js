55fa3b0383cf5e2fea95407bc2b37d4c475621b0
Marcos Rodrigo Jung Alves <digosw@gmail.com>
2022-12-13 08:02:10 -0300

    fix: improve test

--- __tests__/lambda/lambdaEventChecker.test.ts
+++ __tests__/lambda/lambdaEventChecker.test.ts
@@ -7,48 +7,38 @@ describe('invokeEventCheck', () => {
       event: {
         ...lambdaRequestEventExample
       },
-      output: {
-        invoke: false
-      }
+      output: false
     },
     {
       event: {
         ...lambdaInvokeEventExample
       },
-      output: {
-        invoke: true
-      }
+      output: true
     },
     {
       event: {
         ...lambdaRequestEventExample,
         httpMethod: 'POST'
       },
-      output: {
-        invoke: false
-      }
+      output: false
     },
     {
       event: {
         ...lambdaRequestEventExample,
         httpMethod: 'PUT'
       },
-      output: {
-        invoke: false
-      }
+      output: false
     },
     {
       event: {
         ...lambdaRequestEventExample,
         httpMethod: 'PATCH'
       },
-      output: {
-        invoke: false
-      }
+      output: false
     }
   ]
 
   test.each(data)('Should return if the event was invoked or originated by an API Gateway request', (param) => {
-    expect(invokeEventCheck(param.event as {requestContext?: {}})).toStrictEqual(param.output)
+    expect(invokeEventCheck(param.event)).toStrictEqual(param.output)
   })
 })
