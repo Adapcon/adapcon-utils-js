c120a7daea35f726b1019a28241d41c7a38ea85c
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-03-20 08:29:18 -0300

    fix: fix new Date validation

--- src/date/validators.ts
+++ src/date/validators.ts
@@ -1,7 +1,7 @@
-export function isValidSQLDatetime (datetime: string | Date): boolean {
+export function isValidSQLDatetime (datetime: string): boolean {
   const SQLDateTimeFormat = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/
 
-  const isValid = SQLDateTimeFormat.test(String(datetime)) && !!new Date(datetime)
+  const isValid = SQLDateTimeFormat.test(String(datetime)) && !!new Date(datetime).valueOf()
 
   return isValid
 }
