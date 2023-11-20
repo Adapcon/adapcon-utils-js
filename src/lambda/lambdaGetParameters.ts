efbbfb88f627c5ba7c7b122260fcd0f3f92ea873
Marcos Alves <digosw@gmail.com>
2023-11-20 14:45:38 -0300

    refactor: :memo: better readability

--- src/lambda/lambdaGetParameters.ts
+++ src/lambda/lambdaGetParameters.ts
@@ -2,20 +2,18 @@ import { kebabCaseToCamelCase } from './../string/formatters'
 export const lambdaGetParameters = (event: object, eventParams: object): { [key: string]: any } => {
   const fedParams = {}
 
-  for (const element in eventParams) {
-    if (Object.prototype.hasOwnProperty.call(eventParams, element)) {
-      const path: string = eventParams[element]
+  for (const element of Object.keys(eventParams)) {
+    const path: string = eventParams[element]
 
-      if (path in event) {
-        const eventPathObject: object = event[path]
-        if (!eventPathObject) continue
-        const fedParam: any = path === 'body' ? eventPathObject : eventPathObject[element]
-        let paramKey: string = element
-        if (path === 'headers') {
-          paramKey = kebabCaseToCamelCase(element)
-        }
-        fedParams[paramKey] = fedParam
+    if (path in event) {
+      const eventPathObject: object = event[path]
+      if (!eventPathObject) continue
+      const fedParam: any = path === 'body' ? eventPathObject : eventPathObject[element]
+      let paramKey: string = element
+      if (path === 'headers') {
+        paramKey = kebabCaseToCamelCase(element)
       }
+      fedParams[paramKey] = fedParam
     }
   }
 
