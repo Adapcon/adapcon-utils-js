37bc36f0656dc609f5f38b87e697a7b937162b77
Marcos Alves <digosw@gmail.com>
2023-11-20 14:38:41 -0300

    refactor: :memo: deepsource code smell

--- src/lambda/lambdaGetParameters.ts
+++ src/lambda/lambdaGetParameters.ts
@@ -3,17 +3,19 @@ export const lambdaGetParameters = (event: object, eventParams: object): { [key:
   const fedParams = {}
 
   for (const element in eventParams) {
-    const path: string = eventParams[element]
+    if (Object.prototype.hasOwnProperty.call(eventParams, element)) {
+      const path: string = eventParams[element]
 
-    if (path in event) {
-      const eventPathObject: object = event[path]
-      if (!eventPathObject) continue
-      const fedParam: any = path === 'body' ? eventPathObject : eventPathObject[element]
-      let paramKey: string = element
-      if (path === 'headers') {
-        paramKey = kebabCaseToCamelCase(element)
+      if (path in event) {
+        const eventPathObject: object = event[path]
+        if (!eventPathObject) continue
+        const fedParam: any = path === 'body' ? eventPathObject : eventPathObject[element]
+        let paramKey: string = element
+        if (path === 'headers') {
+          paramKey = kebabCaseToCamelCase(element)
+        }
+        fedParams[paramKey] = fedParam
       }
-      fedParams[paramKey] = fedParam
     }
   }
 
