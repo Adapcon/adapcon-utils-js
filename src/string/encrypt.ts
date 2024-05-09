2566dfb47e050ecd20795fbe96bf82766fdb4dc4
viniieng <viniengelmann@gmail.com>
2024-05-09 11:30:28 +0000

    feat: :art: Create function getCipher

--- src/string/encrypt.ts
+++ src/string/encrypt.ts
@@ -1,26 +1,29 @@
 import crypto from 'crypto'
 import { InternalError } from '..'
 
-export function encryptPassword (passwordToEncrypt, encryptionKey) {
+export function encryptPassword (passwordToEncrypt: string, encryptionKey: string) {
   const salt = crypto.randomBytes(16).toString('hex')
-  const key = crypto.scryptSync(encryptionKey, salt, 24)
-  const iv = Buffer.alloc(16, 0)
-  const cipher = crypto.createCipheriv('aes-192-cbc', key, iv)
+  const cipher = getCipher(encryptionKey, salt)
   let encryptedPassword = cipher.update(passwordToEncrypt, 'utf8', 'hex')
   encryptedPassword += cipher.final('hex')
   return `${salt}:${encryptedPassword}`
 }
 
-export function decryptPassword (storedPassword, encryptionKey) {
+export function decryptPassword (storedPassword: string, encryptionKey: string) {
   try {
     const [salt, encryptedPassword] = storedPassword.split(':')
-    const key = crypto.scryptSync(encryptionKey, salt, 24)
-    const iv = Buffer.alloc(16, 0)
-    const decipher = crypto.createDecipheriv('aes-192-cbc', key, iv)
-    let decryptedPassword = decipher.update(encryptedPassword, 'hex', 'utf8')
-    decryptedPassword += decipher.final('utf8')
+    const cipher = getCipher(encryptionKey, salt)
+    let decryptedPassword = cipher.update(encryptedPassword, 'hex', 'utf8')
+    decryptedPassword += cipher.final('utf8')
     return decryptedPassword
   } catch (error) {
     throw new InternalError('Invalid Master Key')
   }
+}
+
+function getCipher (encryptionKey: string, salt: string) {
+  const key = crypto.scryptSync(encryptionKey, salt, 24)
+  const iv = Buffer.alloc(16, 0)
+  const cipher = crypto.createDecipheriv('aes-192-cbc', key, iv)
+  return cipher
 }
