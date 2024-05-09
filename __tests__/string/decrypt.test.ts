d7c44d60868de1c14ba97446bc27862cdbfadd93
viniieng <viniengelmann@gmail.com>
2024-05-09 13:08:28 +0000

    fix: :bug: Refac unitary tests

--- __tests__/string/decrypt.test.ts
+++ __tests__/string/decrypt.test.ts
@@ -1,8 +1,9 @@
-import { decryptPassword } from '../../src/string'
+import { decryptPassword, encryptPassword } from '../../src/string'
 
 describe('decrypt', () => {
   test('Should decrypt String', () => {
-    const result = decryptPassword('b7c8bf5b28f640ee053a4b7879ca12b0:3d4227d5dd4751f022f16a08b582ccc8', 'chave')
+    const hash = encryptPassword('senhavini', 'chave')
+    const result = decryptPassword(hash, 'chave')
     const expected = 'senhavini'
     expect(result).toEqual(expected)
   })
--- __tests__/string/encrypt.test.ts
+++ __tests__/string/encrypt.test.ts
@@ -3,7 +3,6 @@ import { encryptPassword } from '../../src/string'
 describe('encrypt', () => {
   test('Should encrypt String', () => {
     const result = encryptPassword('senhavini', 'chave')
-    const expected = 'b7c8bf5b28f640ee053a4b7879ca12b0:3d4227d5dd4751f022f16a08b582ccc8'
-    expect(result).toEqual(expected)
+    expect(typeof result).toEqual('string')
   })
 })
