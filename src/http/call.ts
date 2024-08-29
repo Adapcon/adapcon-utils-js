652916658e38a79eac3b4605d4491833a6daa287
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-08-29 15:50:33 -0300

    feat: function for http call with retry (#281)
    * chore: npm audit fix and install axios
    
    * feat: implement http call with retry
    
    * feat: improve function docs and types
    
    * fix: explicit undefined return after loop
    
    * chore: relate use of `as` and use axios<T>

---
+++ src/http/call.ts
@@ -0,0 +1,34 @@
+import axios, { type AxiosRequestConfig } from 'axios'
+
+type Options = AxiosRequestConfig & {
+  retryCount?: number
+  timeoutBetweenRetries?: number
+}
+
+/*
+  * @description This function makes an HTTP request to the given URL with the given options.
+  * If the request fails, the function will retry the request up to the given number
+  * of times after the given timout.
+*/
+export async function callHttpWithRetry<T> (
+  url: string,
+  options: Options
+): Promise<T | undefined> {
+  const { retryCount, timeoutBetweenRetries, ...axiosOptions } = options
+  const retries = retryCount ?? 2
+
+  for (let i = 0; i < retries; i++) {
+    try {
+      const { data } = await axios<T>(url, axiosOptions)
+      return data
+    } catch (error) {
+      if (i === retries - 1) {
+        throw error
+      }
+
+      await new Promise(resolve => setTimeout(resolve, timeoutBetweenRetries ?? 500))
+    }
+  }
+
+  return undefined
+}
--- src/http/index.ts
+++ src/http/index.ts
@@ -1,3 +1,4 @@
 export * from './enums'
 export * from './types'
 export * from './http'
+export * from './call'
