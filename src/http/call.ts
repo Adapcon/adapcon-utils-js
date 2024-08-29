22022ff48a81f3adbb9c18ce2dea7408f7189c59
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-08-29 15:34:19 -0300

    feat: improve function docs and types

--- src/http/call.ts
+++ src/http/call.ts
@@ -1,15 +1,26 @@
 import axios, { type AxiosRequestConfig } from 'axios'
 
-type Options = AxiosRequestConfig & { retryCount?: number, timeoutBetweenRetries?: number }
+type Options = AxiosRequestConfig & {
+  retryCount?: number
+  timeoutBetweenRetries?: number
+}
 
-export async function callHttpWithRetry (url: string, options: Options) {
+/*
+  * @description This function makes an HTTP request to the given URL with the given options.
+  * If the request fails, the function will retry the request up to the given number
+  * of times after the given timout.
+*/
+export async function callHttpWithRetry<T> (
+  url: string,
+  options: Options
+): Promise<T | undefined> {
   const { retryCount, timeoutBetweenRetries, ...axiosOptions } = options
   const retries = retryCount ?? 2
 
   for (let i = 0; i < retries; i++) {
     try {
       const { data } = await axios(url, axiosOptions)
-      return data
+      return data as T
     } catch (error) {
       if (i === retries - 1) {
         throw error
