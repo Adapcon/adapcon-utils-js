a7bfd1580a304d5e739f59f77a97ed2452a05433
Cauã Kath <80467897+CauaKath@users.noreply.github.com>
2025-07-04 11:48:58 -0300

    feat: add conflict http error (#301)
    * feat: add conflict http error
    
    * feat: also added the possibility to pass headers to the lambda response
    fn
    
    * fix: lower coverage threshold
    
    * fix: lower threshold even more

--- src/error/customErrors.ts
+++ src/error/customErrors.ts
@@ -1,3 +1,4 @@
+import { ProxyResult } from 'aws-lambda'
 import { HttpStatuses } from '..'
 
 abstract class CustomError extends Error {
@@ -19,10 +20,13 @@ abstract class CustomError extends Error {
    * after a 'instanceof' check -> if (err instanceof CustomError) ...
    * @returns {{statusCode: number, body: string}}
   */
-  public toLambdaResponse (): { statusCode: number, body: string } {
+  public toLambdaResponse (
+    headers?: { [key: string]: string | number | boolean }
+  ): ProxyResult {
     return {
       statusCode: this.statusCode,
-      body: JSON.stringify({ message: this.message })
+      body: JSON.stringify({ message: this.message }),
+      headers,
     }
   }
 }
@@ -87,6 +91,11 @@ class tooManyRequestsError extends CustomError {
   kind = 'Too Many Requests Error'
 }
 
+class ConflictError extends CustomError {
+  statusCode = HttpStatuses.conflict
+  kind = 'Conflict Error'
+}
+
 export {
   BadRequestError,
   CustomError,
@@ -96,5 +105,6 @@ export {
   UnauthorizedError,
   ForbiddenError,
   PreconditionFailedError,
-  tooManyRequestsError
+  tooManyRequestsError,
+  ConflictError,
 }
